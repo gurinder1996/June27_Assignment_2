93591f3a486ca1914ea217c59dfea77eaecacc78
Gurinder Singh <gurindersingh13@outlook.com>
2025-06-30 07:17:43 -0400

    feat: Add consultation request alert to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,5 @@
+const consultationButton = document.getElementById('consultation-button');
 
+consultationButton.addEventListener('click', function() {
+  alert('Thank you for your interest! An AeroGlide concierge will contact you shortly.');
+});
